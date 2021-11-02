4c845845473f670c3622a3abaf1365ce9acb75d9
SoPudge <abcd666@gmail.com>
2021-11-02 20:41:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,11 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-git clone https://github.com/kuoruan/luci-app-kcptun.git ./package/luci-app-kcptun
+#git clone https://github.com/kuoruan/luci-app-kcptun.git ./package/luci-app-kcptun
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git ./package/luci-app-smartdns
-git clone https://github.com/pymumu/openwrt-smartdns.git ./package/smartdns
+#git clone https://github.com/pymumu/openwrt-smartdns.git ./package/smartdns
 
 #replace the new smartdns commit-id
-sed -i 's/0aec326d551925a269a960677f4cd432d8e89385/86902d2e3415666246dae0286262e43923f58532/g' ./package/smartdns/Makefile
-sed -i 's/1.2020.30/1.2020.09.08-2235/g' ./package/smartdns/Makefile
+sed -i 's/a6fe329105c7275d4683d17e95ab9d9f93a9c863/f50e4dd0813da9300580f7188e44ed72a27ae79c/g' ./package/feeds/packages/smartdns/Makefile
+sed -i 's/1.2020.30/1.2021.08.27-1923/g' ./package/feeds/packages/smartdns/Makefile
+sed -i 's/PKG_MIRROR_HASH/#PKG_MIRROR_HASH/g' ./package/feeds/packages/smartdns/Makefile
