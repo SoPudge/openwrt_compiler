d5e7991f8d563e5a19b12128e1acf501e6e399de
SoPudge <abcd666@gmail.com>
2021-11-02 20:56:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,8 @@
 #sed -i 's/PKG_VERSION:=20201010/PKG_VERSION:=20210624/g' ./package/feeds/packages/kcptun/Makefile
 #sed -i 's/PKG_SOURCE_DATE:=2020-10-10/PKG_SOURCE_DATE:=2021-06-24/g' ./package/feeds/packages/kcptun/Makefile
 #sed -i 's/afab2a087b787e59e129c7d4fbc578e1131d264c0da1ce23cd1282321fc3c189/3f39eb2e6ee597751888b710afc83147b429c232591e91bc97565b32895f33a8/g' ./package/feeds/packages/kcptun/Makefile
+
+#replace the new smartdns commit-id
+sed -i 's/a6fe329105c7275d4683d17e95ab9d9f93a9c863/f50e4dd0813da9300580f7188e44ed72a27ae79c/g' ./feeds/packages/net/smartdns/Makefile
+sed -i 's/1.2020.30/1.2021.08.27-1923/g' ./feeds/packages/net/smartdns/Makefile
+sed -i 's/PKG_MIRROR_HASH/#PKG_MIRROR_HASH/g' ./feeds/packages/net/smartdns/Makefile
