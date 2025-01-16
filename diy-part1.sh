5150208e75371fb3f230362f69f07f7af28bcbb2
SoPudge <20067852+SoPudge@users.noreply.github.com>
2025-01-16 22:01:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #git clone https://github.com/kuoruan/luci-app-kcptun.git ./package/luci-app-kcptun
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git ./feeds/luci/applications/
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git ./feeds/luci/applications/
 #git clone https://github.com/pymumu/openwrt-smartdns.git ./package/smartdns
