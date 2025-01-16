556d646f1fe38dcc4789dd857dcf34290736c381
SoPudge <20067852+SoPudge@users.noreply.github.com>
2025-01-16 22:01:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,8 +17,8 @@
 #sed -i 's/afab2a087b787e59e129c7d4fbc578e1131d264c0da1ce23cd1282321fc3c189/3f39eb2e6ee597751888b710afc83147b429c232591e91bc97565b32895f33a8/g' ./package/feeds/packages/kcptun/Makefile
 
 #replace the new smartdns commit-id
-sed -i 's/a202b765e6ce8355335c80214819add3ed72a82426b033d7d5adf1448b415063/af0303d995c3edfa0ce8d0e90abf6846a8ef1b978e7787965001a0597afc122f/g' ./feeds/packages/net/smartdns/Makefile
-sed -i 's/1.2022.38.1/1.2022.40/g' ./feeds/packages/net/smartdns/Makefile
-sed -i 's/9bc857f628299573c7eca0833229d9812b1c1de4/fffe4caf088e4f956eeecb9a3a3fd09a79aca6a9/g' ./feeds/packages/net/smartdns/Makefile
+#sed -i 's/a202b765e6ce8355335c80214819add3ed72a82426b033d7d5adf1448b415063/af0303d995c3edfa0ce8d0e90abf6846a8ef1b978e7787965001a0597afc122f/g' ./feeds/packages/net/smartdns/Makefile
+#sed -i 's/1.2022.38.1/1.2022.40/g' ./feeds/packages/net/smartdns/Makefile
+#sed -i 's/9bc857f628299573c7eca0833229d9812b1c1de4/fffe4caf088e4f956eeecb9a3a3fd09a79aca6a9/g' ./feeds/packages/net/smartdns/Makefile
 #avahi-glib for cups-filters
 #sed -i 's/disable-glib/enable-glib/g' ./feeds/packages/libs/avahi/Makefile
